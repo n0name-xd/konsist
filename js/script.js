29728fa097e540c3f18efa1b621f0c7146923144
n0name_ <n0name-xd@inbox.ru>
2022-03-24 19:17:21 +0300

    Перенес все оперирование JavaScrip(ом) в фаил JS, Создал переменную для document.querySelector(.burger)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-function openNav() {
-    document.querySelector(".burger").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.querySelector(".burger").style.width = "0%";
-  }
+const burger = document.querySelector(".burger");
+
+const closeNav = () => burger.style.width = "0%";
+const openNav = () => burger.style.width = "100%";
+
+const opennav = document.querySelector(".opennav");
+opennav.addEventListener("click", openNav);
+
+const closebtn = document.querySelector(".closebtn");
+closebtn.addEventListener("click", closeNav);
+
+const home = document.querySelector(".h");
+home.addEventListener("click", closeNav);
+
+const about = document.querySelector(".a");
+about.addEventListener("click", closeNav);
+
+const skills = document.querySelector(".s");
+skills.addEventListener("click", closeNav);
+
+const portfolio = document.querySelector(".p");
+portfolio.addEventListener("click", closeNav);
+
+const contacts = document.querySelector(".c");
+contacts.addEventListener("click", closeNav);
+
+
+
+
+
